40cab8ae3d967e7bed110a2297a9d6438471769e
tkachovvv <dmitiy.tk.skyeng@outlook.com>
2022-10-31 18:22:24 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int clientOS = 1;
 
+        switch (clientOS) {
+            case 0:
+                System.out.println("Установите приложение для iOS по ссылке");
+                break;
+            case 1:
+                System.out.println("Установите приложение для Android по ссылке");
+                break;
+            default:
+                System.out.println("Ваша операционная система не может быть распознана");
+        }
     }
 }
