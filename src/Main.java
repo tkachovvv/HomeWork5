7734e2fe25f7e80f70de6ee96f61015e989f1102
tkachovvv <dmitiy.tk.skyeng@outlook.com>
2022-11-02 00:16:05 +0300

    Task 5

--- src/Main.java
+++ src/Main.java
@@ -43,6 +43,31 @@ public class Main {
             System.out.println("Срок доставки составит: " + deliveryTerm * 3 + " суток.");
 
         }
+        int monthNumber = 4;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
+        }
     }
 }
 
